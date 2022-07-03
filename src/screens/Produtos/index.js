3b8020a1ebad88227c0f3ec70952827022657cc2
thiagocarvalho93 <thiagopaes1993@gmail.com>
2022-07-03 14:52:43 -0300

    Layout produtos

--- src/screens/Produtos/index.js
+++ src/screens/Produtos/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, FlatList, Image } from "react-native";
+import { View, Text, Button, FlatList, Image, TextInput } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { Logout } from "../../util/Logout";
 import { styles } from "./styles";
@@ -20,38 +20,36 @@ export const Produtos = () => {
   return (
     <>
       <View style={styles.containter}>
-        <Logout />
-        <Text>Produtos</Text>
-        <Text style={{ marginBottom: 10 }}>Produtos</Text>
-        <FlatList
-          data={listaProdutos.data}
-          showsVerticalScrollIndicator={true}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                alignItems: "center",
-                backgroundColor: "white",
-                marginBottom: 10,
-                borderRadius: 10,
-                borderWidth: 3,
-                borderColor: "indigo",
-              }}
-            >
-              <Text
-                style={{ borderBottomWidth: 2, borderBottomColor: "indigo" }}
-              >
-                {item.nomeProduto}{" "}
-              </Text>
-              <Text>R$ {item.valorUnitario} </Text>
-              <Text>{item.categoria.categoria} </Text>
-              <Image
-                source={item.urlFoto}
-                style={{ width: 200, height: 200 }}
-              />
-            </View>
-          )}
-          keyExtractor={(item) => item.idProduto}
-        />
+        <View style={styles.headerContainer}>
+          <Logout />
+          <Text style={styles.title}>Produtos</Text>
+          <View style={styles.pesquisaContainer}>
+            <TextInput placeholder="Buscar" style={styles.input}></TextInput>
+            <Text style={styles.subTitle}>Categoria: x</Text>
+          </View>
+        </View>
+        <View style={styles.produtosContainer}>
+          <FlatList
+            data={listaProdutos.data}
+            showsVerticalScrollIndicator={true}
+            renderItem={({ item }) => (
+              <View style={styles.cardProduto}>
+                <Text
+                  style={{ borderBottomWidth: 2, borderBottomColor: "indigo" }}
+                >
+                  {item.nomeProduto}{" "}
+                </Text>
+                <Text>R$ {item.valorUnitario} </Text>
+                <Text>{item.categoria.categoria} </Text>
+                <Image
+                  source={item.urlFoto}
+                  style={{ width: 200, height: 200 }}
+                />
+              </View>
+            )}
+            keyExtractor={(item) => item.idProduto}
+          />
+        </View>
       </View>
     </>
   );
