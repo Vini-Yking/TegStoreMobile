22194f49c257d48e488870bbee1d4b3e8c3c528b
Wendel <wendel_perroud@hotmail.com>
2022-07-03 22:38:00 -0300

    adicionado icone da tab home

--- src/routes/Tab.js
+++ src/routes/Tab.js
@@ -3,13 +3,32 @@ import { Home } from "../screens/Home";
 import { Produtos } from "../screens/Produtos";
 import { Clientes } from "../screens/Clientes";
 import { Perfil } from "../screens/Perfil";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faUserTie } from '@fortawesome/free-solid-svg-icons/faUserTie';
+import { faCircle } from '@fortawesome/free-solid-svg-icons/faCircle';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons/faHouseChimney';
+
 
 
 const MyTab = createBottomTabNavigator();
 
 const Tab = () => {
   return (
-    <MyTab.Navigator>
+    <MyTab.Navigator
+    screenOptions={({route}) =>({
+      tabBarIcon: ({focused, color, size }) =>{
+        let iconName;
+
+        if(route.name === "Home"){
+          iconName = faHouseChimney;
+        }
+
+        return <FontAwesomeIcon icon={iconName} size={size} color={color} />
+      },
+      tabBarActiveTintColor: 'indigo',
+      tabBarInactiveTintColor: 'gray'
+    })}
+    >
       <MyTab.Screen name="Home" component={Home} options={{headerShown: false}} />
       <MyTab.Screen name="Produtos" component={Produtos} options={{headerShown: false}}/>
       <MyTab.Screen name="Clientes" component={Clientes} options={{headerShown: false}}/>
