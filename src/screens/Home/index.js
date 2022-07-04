ae7078c79b7ae9487e20634aa9fc9cb382ab125b
Hítalo <hitalo.taverna.quintal@gmail.com>
2022-07-04 17:17:59 -0300

    carousel

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -5,17 +5,20 @@ import { BotaoLogout } from "../../components/BotaoLogout";
 import CardCategoria from "./components/CardCategoria";
 import Sobre from "./components/Sobre";
 import { styles } from "./styles";
+import { MyCarousel } from "./components/carousel";
 
 export const Home = () => {
   return (
     <ScrollView>
       <BotaoLogout />
-      <View style={styles.categoriasContainer}>
+       <ScrollView style={[styles.categoriasContainer, {flexDirection: 'row'}]}>
         <CardCategoria nomeCategoria="Nome da Categoria" />
-      </View>
+      </ScrollView> 
       <View style={styles.sobreContainer}>
         <Sobre />
       </View>
+      <MyCarousel/>
     </ScrollView>
+
   );
 };
