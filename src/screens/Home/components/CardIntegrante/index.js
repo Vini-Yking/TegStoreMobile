3f1f830f89f142f24b63ddf15e2c5f58c87002c3
thiagocarvalho93 <thiagopaes1993@gmail.com>
2022-07-04 19:43:33 -0300

    Links para redes sociais

--- src/screens/Home/components/CardIntegrante/index.js
+++ src/screens/Home/components/CardIntegrante/index.js
@@ -1,12 +1,46 @@
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
-import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+  Linking,
+} from "react-native";
 import AppStyles from "../../../../themes/AppStyles";
 
 import { styles } from "./styles";
+import { useCallback } from "react";
 
-const CardIntegrante = ({ nome, urlFoto }) => {
+const CardIntegrante = ({ nome, urlFoto, urlLinkedin, urlGithub }) => {
+  const handleLinkedin = useCallback(async () => {
+    // Checking if the link is supported for links with custom URL scheme.
+    const supported = await Linking.canOpenURL(urlLinkedin);
+
+    if (supported) {
+      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+      // by some browser in the mobile
+      await Linking.openURL(urlLinkedin);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${urlLinkedin}`);
+    }
+  }, [urlLinkedin]);
+
+  const handleGithub = useCallback(async () => {
+    // Checking if the link is supported for links with custom URL scheme.
+    const supported = await Linking.canOpenURL(urlGithub);
+
+    if (supported) {
+      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+      // by some browser in the mobile
+      await Linking.openURL(urlGithub);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${urlGithub}`);
+    }
+  }, [urlGithub]);
+
   return (
     <View style={[{ paddingVertical: 13, width: 150 }]}>
       <Image source={{ uri: urlFoto }} style={styles.foto} />
@@ -19,10 +53,10 @@ const CardIntegrante = ({ nome, urlFoto }) => {
         {nome}
       </Text>
       <View style={styles.redesSociais}>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button} onPress={handleLinkedin}>
           <FontAwesomeIcon icon={faLinkedin} size={20} color="white" />
         </Pressable>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button} onPress={handleGithub}>
           <FontAwesomeIcon icon={faGithub} size={20} color="white" />
         </Pressable>
       </View>
