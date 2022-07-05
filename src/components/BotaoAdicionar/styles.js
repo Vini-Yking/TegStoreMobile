266da4afd5e70f0a257cd0dc6557ae38bd0f221b
thiagocarvalho93 <thiagopaes1993@gmail.com>
2022-07-05 19:35:37 -0300

    Pesquisa com scroll infinito e botão adicionar

---
+++ src/components/BotaoAdicionar/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "green",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+});
