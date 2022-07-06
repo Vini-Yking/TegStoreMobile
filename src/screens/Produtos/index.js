e632203d843b1e75597501159484142c87c892df
thiagocarvalho93 <thiagopaes1993@gmail.com>
2022-07-05 22:03:57 -0300

    scroll resolvido falta pesquisa

--- src/screens/Produtos/index.js
+++ src/screens/Produtos/index.js
@@ -5,6 +5,7 @@ import {
   TextInput,
   SafeAreaView,
   ActivityIndicator,
+  Pressable,
 } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { BotaoLogout } from "../../components/BotaoLogout/index.js";
@@ -19,59 +20,49 @@ import AppStyles from "../../themes/AppStyles";
 import { CardProduto } from "./components/CardProduto";
 import { noAuto } from "@fortawesome/fontawesome-svg-core";
 import { BotaoAdicionar } from "../../components/BotaoAdicionar/index.js";
+import { FooterList } from "./components/FooterList/index.js";
 
 export const Produtos = ({ navigation }) => {
   const [listaProdutos, setListaProdutos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [pesquisa, setPesquisa] = useState("");
+  const [nomeProduto, setNomeProduto] = useState("");
   const { categorias } = useContext(AuthContext);
   const [page, setPage] = useState(0);
 
-  // AJEITAR PAGEABLE DA API
-  const handlePesquisa = async () => {
-    const pageSize = 10;
-    setListaProdutos([]);
-    if (pesquisa.length === 0) {
+  const handleBuscaPaginada = async () => {
+    if (loading) return;
+    setLoading(true);
+    const pageSize = 15;
+    if (nomeProduto.length === 0) {
       // Pega todos
       const produtos = await getAllProdutosPaginados(page, pageSize);
-      setListaProdutos([...listaProdutos, ...produtos.data.content]);
-      console.log(produtos);
       console.log(page);
+      setListaProdutos([...listaProdutos, ...produtos.data.content]);
       setPage((page) => page + 1);
+      setLoading(false);
       return;
     }
     // Filtra pelo nome
-    const produtosByName = await getProdutoByName(pesquisa, page, pageSize);
+    setListaProdutos([]);
+    console.log(nomeProduto);
+    const produtosByName = await getProdutoByName(nomeProduto, page, pageSize);
+    console.log(produtosByName);
     setListaProdutos([...listaProdutos, ...produtosByName.data.content]);
     console.log(listaProdutos);
     setPage((page) => page + 1);
+    setLoading(false);
   };
 
-  // Pesquisa novo produto
-  useEffect(() => {
-    setLoading(true);
+  const handlePesquisa = () => {
+    setNomeProduto(pesquisa);
     setPage(0);
-    handlePesquisa();
-    setLoading(false);
-  }, [pesquisa]);
+    handleBuscaPaginada();
+  };
 
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     setLoading(true);
-  //     if (pesquisa.length === 0) {
-  //       // Pega todos
-  //       const produtos = await getAllProdutos();
-  //       setListaProdutos(produtos.data);
-  //       setLoading(false);
-  //       return;
-  //     }
-  //     // Filtra pelo nome
-  //     const produtosByName = await getProdutoByName(pesquisa, 0, 4);
-  //     setListaProdutos(produtosByName.data.content);
-  //     setLoading(false);
-  //   };
-  //   getProducts();
-  // }, [pesquisa]);
+  useEffect(() => {
+    handleBuscaPaginada();
+  }, []);
 
   const handleNavigation = (item) => {
     navigation.navigate("DetalhesProduto", {
@@ -87,7 +78,7 @@ export const Produtos = ({ navigation }) => {
 
   const handleDelete = async (item) => {
     const response = await deleteProduto(item.idProduto);
-    Alert.alert("Produto excluir com sucesso!");
+    Alert.alert("Produto excluido com sucesso!");
     navigation.navigate("Produtos");
   };
 
@@ -105,32 +96,32 @@ export const Produtos = ({ navigation }) => {
               value={pesquisa}
               onChangeText={setPesquisa}
             ></TextInput>
+            <Pressable onPress={handlePesquisa}>
+              <Text>Pesquisar</Text>
+            </Pressable>
           </View>
         </View>
         <SafeAreaView style={styles.produtosContainer}>
-          {loading ? (
-            <ActivityIndicator style={{ alignSelf: "center", marginTop: 20 }} />
-          ) : (
-            <FlatList
-              style={{ width: "100%" }}
-              data={listaProdutos}
-              showsVerticalScrollIndicator={true}
-              onEndReached={handlePesquisa}
-              renderItem={({ item }) => (
-                <CardProduto
-                  nomeProduto={item.nomeProduto}
-                  valorUnitario={item.valorUnitario}
-                  categoria={item.categoria.categoria}
-                  urlFoto={item.urlFoto}
-                  item={item}
-                  navigation={() => handleNavigation(item)}
-                  handleEditar={handleEditar}
-                  handleRemover={handleDelete}
-                />
-              )}
-              keyExtractor={(item) => String(item.idProduto)}
-            />
-          )}
+          <FlatList
+            style={{ width: "100%" }}
+            data={listaProdutos}
+            showsVerticalScrollIndicator={true}
+            onEndReached={handleBuscaPaginada}
+            renderItem={({ item }) => (
+              <CardProduto
+                nomeProduto={item.nomeProduto}
+                valorUnitario={item.valorUnitario}
+                categoria={item.categoria.categoria}
+                urlFoto={item.urlFoto}
+                item={item}
+                navigation={() => handleNavigation(item)}
+                handleEditar={handleEditar}
+                handleRemover={handleDelete}
+              />
+            )}
+            keyExtractor={(item) => String(item.idProduto)}
+            ListFooterComponent={<FooterList load={loading} />}
+          />
         </SafeAreaView>
       </View>
     </>
