be710a18bcb81df325a0a6ec45736598a09b7952
Wendel <wendel_perroud@hotmail.com>
2022-07-07 13:45:54 -0300

    coreções modais

--- src/components/ModalErro/styles.js
+++ src/components/ModalErro/styles.js
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   centeredView: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
